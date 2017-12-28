ad3eee7217cd9e987698ff190154f5183cf974f0
justinreock <justin.reock@openlogic.com>
2017-12-28 18:57:20 -0500

    Add ListCollectionIds

--- firestore/examples/end2end/src/apimethods/listcollectionids.go
+++ firestore/examples/end2end/src/apimethods/listcollectionids.go
@@ -1,9 +1,35 @@
 package apimethods
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"fsutils"
+	firestore "go-genproto/googleapis/firestore/v1beta1"
+)
 
 //ListCollectionIds ... List all collection IDs from a database
 func ListCollectionIds() {
-	fmt.Println(":: Listing All Collection Ids ::")
+	fmt.Println("\n:: Listing All Collection Ids ::\n")
+
+	client, conn := fsutils.MakeFSClient()
+
+	defer conn.Close()
+
+	listCollectionIdReq := firestore.ListCollectionIdsRequest{
+		Parent: "projects/firestoretestclient/databases/(default)",
+	}
+
+	resp, err := client.ListCollectionIds(context.Background(), &listCollectionIdReq)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, cid := range resp.CollectionIds {
+		fmt.Println(cid)
+	}
+
 	return
+
 }
