5ad093eea8b46c75ce6ad406382b4e4a19a2f17f
justinreock <justin.reock@openlogic.com>
2017-12-29 15:23:50 -0500

    Add DeleteIndex, attach ASF2.0 License

--- firestore/examples/end2end/src/apimethods/deleteindex.go
+++ firestore/examples/end2end/src/apimethods/deleteindex.go
@@ -1,9 +1,35 @@
 package apimethods
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"fsutils"
+	admin "go-genproto/googleapis/firestore/admin/v1beta1"
+	"userutil"
+)
 
 //DeleteIndex ... Delete an Index from the database
 func DeleteIndex() {
-	fmt.Println(":: Deleting an Index ::")
-	return
+	fmt.Println("\n:: Deleting an Index ::\n")
+
+	client, conn := fsutils.MakeFSAdminClient()
+
+	defer conn.Close()
+
+	fmt.Print("Enter Index Name: ")
+	indexName := "projects/firestoretestclient/databases/(default)/indexes/" + userutil.ReadFromConsole()
+
+	delIndexReq := admin.DeleteIndexRequest{
+		Name: indexName,
+	}
+
+	_, err := client.DeleteIndex(context.Background(), &delIndexReq)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("\nFinished deleting index!\n")
+
 }
