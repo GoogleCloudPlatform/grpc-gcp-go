7015ba5ff63bac7e74d5055c90ae2873a4a3ee5c
Weiran Fang <weiranf@google.com>
2020-01-31 14:14:56 -0800

    Add different methods

--- e2e-examples/gcs/README.md
+++ e2e-examples/gcs/README.md
@@ -1,20 +1,31 @@
 ## Example commands
 
-Use grpc API for reading 200mb gcs object through CFE:
+Use grpc API for reading a gcs object through CFE:
 
 ```sh
-go run main.go -bkt=gcs-grpc-team-weiranf -obj=200mb -calls=50
+go run main.go -bkt=gcs-grpc-team-weiranf -obj=200mb -method=media -calls=50
 ```
 
-Use grpc API for reading 200mb gcs object through DirectPath (VM needs to be dp
-enabled):
+Use grpc API for reading metadata of a gcs object through CFE:
+
+```sh
+go run main.go -bkt=gcs-grpc-team-weiranf -obj=200mb -method=metadata -calls=50
+```
+
+Use grpc API for writing a 200mb gcs object through CFE:
+
+```sh
+go run main.go -bkt=gcs-grpc-team-weiranf -obj=grpc-write-200mb -method=write -size=204800 -calls=50
+```
+
+To use DirectPath, add the arg `-dp` (VM needs to be dp enabled), for example:
 
 ```sh
 go run main.go -bkt=gcs-grpc-team-weiranf -obj=200mb -calls=50 -dp
 ```
 
-Use HTTP JSON API for reading 200mb gcs object through CFE:
+To use HTTP JSON API, add the arg `-http`, for example:
 
 ```sh
-go run main.go -bkt=gcs-grpc-team-weiranf -obj=200mb -calls=50 -http
+go run main.go -bkt=gcs-grpc-team-weiranf -obj=200mb -method=media -calls=50 -http
 ```
--- e2e-examples/gcs/main.go
+++ e2e-examples/gcs/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"hash/crc32"
 	"io"
 	"io/ioutil"
 	"os"
@@ -18,6 +19,7 @@ import (
 	"cloud.google.com/go/storage"
 
 	gcspb "github.com/GoogleCloudPlatform/grpc-gcp-go/e2e-examples/gcs/google.golang.org/genproto/googleapis/storage/v1"
+	wrappers "github.com/golang/protobuf/ptypes/wrappers"
 	_ "google.golang.org/grpc/balancer/grpclb"
 	grpcgoogle "google.golang.org/grpc/credentials/google"
 	"google.golang.org/grpc/credentials/oauth"
@@ -37,6 +39,8 @@ var (
 	numCalls   = flag.Int("calls", 1, "num of calls")
 	uploadSize = flag.Int("upload", 0, "upload size in kb")
 	cookie     = flag.String("cookie", "", "cookie header")
+	method     = flag.String("method", "media", "method names")
+	size       = flag.Int("size", 0, "write size in kb")
 )
 
 func upload(client *storage.Client, kb int) {
@@ -112,66 +116,196 @@ func makeGrpcRequest(client gcspb.StorageClient) []int {
 	fmt.Println("========================== start grpc calls ===============================")
 	res := []int{}
 
-	req := gcspb.GetObjectMediaRequest{
-		Bucket: *bucketName,
-		Object: *objectName,
-	}
-
-	for i := 0; i < *numCalls; i++ {
-		ctx := context.Background()
-		if i == *numCalls-1 {
-			md := metadata.Pairs("cookie", *cookie)
-			ctx = metadata.NewOutgoingContext(ctx, md)
+	switch *method {
+	case "media":
+		req := gcspb.GetObjectMediaRequest{
+			Bucket: *bucketName,
+			Object: *objectName,
 		}
 
-		start := time.Now()
-		stream, err := client.GetObjectMedia(ctx, &req)
-		if err != nil {
-			fmt.Println("GetObjectMedia got error: ", err)
-			os.Exit(1)
-		}
-
-		for {
-			_, err := stream.Recv()
-			if err == io.EOF {
-				break
+		for i := 0; i < *numCalls; i++ {
+			ctx := context.Background()
+			if i == *numCalls-1 {
+				md := metadata.Pairs("cookie", *cookie)
+				ctx = metadata.NewOutgoingContext(ctx, md)
 			}
+
+			start := time.Now()
+			stream, err := client.GetObjectMedia(ctx, &req)
 			if err != nil {
-				fmt.Println("stream.Recv() got error: ", err)
+				fmt.Println("GetObjectMedia got error: ", err)
 				os.Exit(1)
 			}
-			//fmt.Printf("rsp: %+v\n", rsp)
+
+			for {
+				_, err := stream.Recv()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					fmt.Println("stream.Recv() got error: ", err)
+					os.Exit(1)
+				}
+				//fmt.Printf("rsp: %+v\n", rsp)
+			}
+			total := time.Since(start).Milliseconds()
+			res = append(res, int(total))
+			fmt.Println("total time in ms for GetObjectMedia: ", total)
 		}
-		total := time.Since(start).Milliseconds()
-		res = append(res, int(total))
-		fmt.Println("total time in ms for GetObjectMedia: ", total)
+	case "metadata":
+		req := gcspb.GetObjectRequest{
+			Bucket: *bucketName,
+			Object: *objectName,
+		}
+		for i := 0; i < *numCalls; i++ {
+			ctx := context.Background()
+			start := time.Now()
+			_, err := client.GetObject(ctx, &req)
+			if err != nil {
+				fmt.Println("GetObject got error: ", err)
+				os.Exit(1)
+			}
+			//fmt.Printf("-----> GetObject result: %+v\n", obj)
+			total := time.Since(start).Milliseconds()
+			res = append(res, int(total))
+			fmt.Println("total time in ms for GetObjectMedia: ", total)
+		}
+	case "write":
+		totalBytes := *size * 1024
+
+		for i := 0; i < *numCalls; i++ {
+			ctx := context.Background()
+			start := time.Now()
+			stream, err := client.InsertObject(ctx)
+			if err != nil {
+				fmt.Println("InsertObject got error: ", err)
+				os.Exit(1)
+			}
+
+			offset := 0
+			isFirst := true
+			isLast := false
+
+			for offset < totalBytes {
+				//fmt.Printf("-----> offset: %v\n", offset)
+				var add int
+				if offset+int(gcspb.ServiceConstants_MAX_WRITE_CHUNK_BYTES) <= totalBytes {
+					add = int(gcspb.ServiceConstants_MAX_WRITE_CHUNK_BYTES)
+				} else {
+					add = totalBytes - offset
+				}
+				if offset+add == totalBytes {
+					isLast = true
+				}
+				buf := make([]byte, add, add)
+				//fmt.Printf("-----> buf: %v\n", buf)
+				req := getInsertRequest(isFirst, isLast, int64(offset), buf)
+				//fmt.Printf("-----> req: %+v\n", req)
+				if err := stream.Send(req); err != nil {
+					fmt.Println("stream.Send got error: ", err)
+				}
+				isFirst = false
+				offset += add
+			}
+			stream.CloseAndRecv()
+
+			total := time.Since(start).Milliseconds()
+			//fmt.Printf("-----> GetObject result: %+v\n", obj)
+			res = append(res, int(total))
+			fmt.Println("total time in ms for streaming write: ", total)
+		}
 	}
 	return res
+}
+
+func getInsertRequest(isFirst bool, isLast bool, offset int64, buf []byte) *gcspb.InsertObjectRequest {
+	req := &gcspb.InsertObjectRequest{}
+	if isFirst {
+		req.FirstMessage = &gcspb.InsertObjectRequest_InsertObjectSpec{
+			InsertObjectSpec: &gcspb.InsertObjectSpec{
+				Resource: &gcspb.Object{
+					Bucket: *bucketName,
+					Name:   *objectName,
+				},
+			},
+		}
+	}
+	crc32c := crc32.MakeTable(crc32.Castagnoli)
+	req.Data = &gcspb.InsertObjectRequest_ChecksummedData{
+		ChecksummedData: &gcspb.ChecksummedData{
+			Content: buf,
+			Crc32C: &wrappers.UInt32Value{
+				Value: crc32.Checksum(buf, crc32c),
+			},
+		},
+	}
+	req.WriteOffset = offset
+	if isLast {
+		req.FinishWrite = true
+	}
+	return req
 }
 
 func makeJsonRequest(client *storage.Client) []int {
 	fmt.Println("========================== start http calls ===============================")
 	res := []int{}
 
-	for i := 0; i < *numCalls; i++ {
-		start := time.Now()
-		obj := client.Bucket(*bucketName).Object(*objectName)
-		rc, err := obj.NewReader(context.Background())
-		if err != nil {
-			fmt.Println("Failed to create object reader: %v", err)
-			os.Exit(1)
-		}
-		defer rc.Close()
+	switch *method {
+	case "media":
+		for i := 0; i < *numCalls; i++ {
+			start := time.Now()
+			obj := client.Bucket(*bucketName).Object(*objectName)
+			rc, err := obj.NewReader(context.Background())
+			if err != nil {
+				fmt.Println("Failed to create object reader: %v", err)
+				os.Exit(1)
+			}
+			defer rc.Close()
 
-		_, err = ioutil.ReadAll(rc)
-		if err != nil {
-			fmt.Println("Failed to read data from object: %v", err)
-			os.Exit(1)
+			_, err = ioutil.ReadAll(rc)
+			if err != nil {
+				fmt.Println("Failed to read data from object: %v", err)
+				os.Exit(1)
+			}
+			total := time.Since(start).Milliseconds()
+			res = append(res, int(total))
+			//fmt.Printf("http object data: %s\n", data)
+			fmt.Println("total time in ms for read: ", total)
 		}
-		total := time.Since(start).Milliseconds()
-		res = append(res, int(total))
-		//fmt.Printf("http object data: %s\n", data)
-		fmt.Println("total time in ms for http call: ", total)
+	case "metadata":
+		for i := 0; i < *numCalls; i++ {
+			ctx := context.Background()
+			start := time.Now()
+			obj := client.Bucket(*bucketName).Object(*objectName)
+			_, err := obj.Attrs(ctx)
+			if err != nil {
+				fmt.Println("obj.Attrs() got error: ", err)
+				os.Exit(1)
+			}
+			//fmt.Printf("-----> obj.Attrs() result: %+v\n", attr)
+			total := time.Since(start).Milliseconds()
+			res = append(res, int(total))
+			fmt.Println("total time in ms for read attributes: ", total)
+		}
+	case "write":
+		for i := 0; i < *numCalls; i++ {
+			ctx := context.Background()
+			start := time.Now()
+			msg := strings.Repeat("x", *size*1024)
+			obj := client.Bucket(*bucketName).Object(*objectName)
+			w := obj.NewWriter(ctx)
+			if _, err := fmt.Fprint(w, msg); err != nil {
+				fmt.Println("Failed to write message to object: %v", err)
+			}
+			if err := w.Close(); err != nil {
+				fmt.Println("object writer failed closing: %v", err)
+				os.Exit(1)
+			}
+			total := time.Since(start).Milliseconds()
+			res = append(res, int(total))
+			fmt.Println("total time in ms for http write: ", total)
+		}
+
 	}
 	return res
 }
