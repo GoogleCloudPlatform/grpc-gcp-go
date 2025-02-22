93d372d38a1e2bb9a92cb876c102d1a04b8c5c46
Bilun Zhang <zbilun@google.com>
2025-02-22 19:43:30 +0000

    Revert the update of the label detector.

--- continuous_load_testing/deploy.sh
+++ continuous_load_testing/deploy.sh
@@ -4,15 +4,6 @@ ROOT_DIR=$(dirname $(dirname $(pwd)))
 echo $WORKING_DIR
 echo $ROOT_DIR
 
-METHODS="HalfDuplexCall"
-
-if [ ! -z "$1" ]; then
-    METHODS=$1
-    echo "Using custom methods: $METHODS"
-else
-    echo "Using default methods: $METHODS"
-fi
-export METHODS
 
 go generate && go build
 kubectl delete deployment client-go-manual
@@ -23,6 +14,3 @@ gcloud artifacts docker images delete us-docker.pkg.dev/directpathgrpctesting-cl
 docker push us-docker.pkg.dev/directpathgrpctesting-client/directpathgrpctesting-client/directpathgrpctesting-client-go-manual
 gcloud container clusters get-credentials cluster-1 --region us-west1 --project directpathgrpctesting-client
 kubectl apply -f client-go-manual.yaml
-
-
-echo "Running the Go application with methods: $METHODS"
