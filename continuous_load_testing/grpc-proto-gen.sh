1be536531dd9e7294089e66c87da06dc0f19d8ba
Bilun Zhang <zbilun@google.com>
2025-05-19 08:12:31 +0000

    Add grpc_gcp own test proto

--- continuous_load_testing/grpc-proto-gen.sh
+++ continuous_load_testing/grpc-proto-gen.sh
@@ -1,17 +1,17 @@
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 export PATH="$PATH:$(go env GOPATH)/bin"
 mkdir -p proto
-protoc --proto_path=../third_party/grpc-proto \
+protoc --proto_path=proto \
 --go_out=proto \
 --go_opt=module=continuous_load_testing/proto \
---go_opt=Mgrpc/testing/empty.proto=continuous_load_testing/proto/grpc/testing/empty \
---go_opt=Mgrpc/testing/messages.proto=continuous_load_testing/proto/grpc/testing/messages \
---go_opt=Mgrpc/testing/test.proto=continuous_load_testing/proto/grpc/testing/test \
+--go_opt=Mgrpc/testing/empty.proto=continuous_load_testing/proto/grpc_gcp/testing/empty \
+--go_opt=Mgrpc/testing/messages.proto=continuous_load_testing/proto/grpc_gcp/testing/messages \
+--go_opt=Mgrpc/testing/test.proto=continuous_load_testing/proto/grpc_gcp/testing/test \
 --go-grpc_out=proto \
 --go-grpc_opt=module=continuous_load_testing/proto \
---go-grpc_opt=Mgrpc/testing/empty.proto=continuous_load_testing/proto/grpc/testing/empty \
---go-grpc_opt=Mgrpc/testing/messages.proto=continuous_load_testing/proto/grpc/testing/messages \
---go-grpc_opt=Mgrpc/testing/test.proto=continuous_load_testing/proto/grpc/testing/test \
-grpc/testing/empty.proto \
-grpc/testing/messages.proto \
-grpc/testing/test.proto
+--go-grpc_opt=Mgrpc/testing/empty.proto=continuous_load_testing/proto/grpc_gcp/testing/empty \
+--go-grpc_opt=Mgrpc/testing/messages.proto=continuous_load_testing/proto/grpc_gcp/testing/messages \
+--go-grpc_opt=Mgrpc/testing/test.proto=continuous_load_testing/proto/grpc_gcp/testing/test \
+grpc_gcp/testing/empty.proto \
+grpc_gcp/testing/messages.proto \
+grpc_gcp/testing/test.proto
