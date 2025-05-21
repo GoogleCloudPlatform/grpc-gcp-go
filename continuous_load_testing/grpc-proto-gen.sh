1910ecf2b0bfb673d08ce21d995a25d9e5b5051d
Bilun Zhang <zbilun@google.com>
2025-05-21 23:26:24 +0000

    Updated grpc-proto-gen code.

--- continuous_load_testing/grpc-proto-gen.sh
+++ continuous_load_testing/grpc-proto-gen.sh
@@ -4,14 +4,14 @@ mkdir -p proto
 protoc --proto_path=proto \
 --go_out=proto \
 --go_opt=module=continuous_load_testing/proto \
---go_opt=Mgrpc/testing/empty.proto=continuous_load_testing/proto/grpc_gcp/testing/empty \
---go_opt=Mgrpc/testing/messages.proto=continuous_load_testing/proto/grpc_gcp/testing/messages \
---go_opt=Mgrpc/testing/test.proto=continuous_load_testing/proto/grpc_gcp/testing/test \
+--go_opt=Mgrpc_gcp/testing/empty.proto=continuous_load_testing/proto/grpc_gcp/testing/empty \
+--go_opt=Mgrpc_gcp/testing/messages.proto=continuous_load_testing/proto/grpc_gcp/testing/messages \
+--go_opt=Mgrpc_gcp/testing/test.proto=continuous_load_testing/proto/grpc_gcp/testing/test \
 --go-grpc_out=proto \
 --go-grpc_opt=module=continuous_load_testing/proto \
---go-grpc_opt=Mgrpc/testing/empty.proto=continuous_load_testing/proto/grpc_gcp/testing/empty \
---go-grpc_opt=Mgrpc/testing/messages.proto=continuous_load_testing/proto/grpc_gcp/testing/messages \
---go-grpc_opt=Mgrpc/testing/test.proto=continuous_load_testing/proto/grpc_gcp/testing/test \
+--go-grpc_opt=Mgrpc_gcp/testing/empty.proto=continuous_load_testing/proto/grpc_gcp/testing/empty \
+--go-grpc_opt=Mgrpc_gcp/testing/messages.proto=continuous_load_testing/proto/grpc_gcp/testing/messages \
+--go-grpc_opt=Mgrpc_gcp/testing/test.proto=continuous_load_testing/proto/grpc_gcp/testing/test \
 grpc_gcp/testing/empty.proto \
 grpc_gcp/testing/messages.proto \
 grpc_gcp/testing/test.proto
